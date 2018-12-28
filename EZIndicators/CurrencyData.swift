264de3f8a33dfcc0bfd366d5e86e9708d1cc5e72
Sean Goldsborough <smgoldsborough@gmail.com>
2018-12-28 14:59:03 -0500

    feat: adds code for data model object

--- EZIndicators/CurrencyData.swift
+++ EZIndicators/CurrencyData.swift
@@ -8,3 +8,26 @@
 
 import Foundation
 // TODO: put a shared instance singleton struct data model here
+class CoinData : NSObject {
+    public var openTime: Double? = 0.0
+    public var o: Double? = 0.0
+    public var h: Double? = 0.0
+    public var l: Double? = 0.0
+    public var c: Double? = 0.0
+    public var v: Double? = 0.0
+    public var closeTime: Double? = 0.0
+    public var gav: Double? = 0.0
+    public var numTrades: Double? = 0.0
+    public var takerBaseVol: Double? = 0.0
+    public var takerQuoteVol: Double? = 0.0
+    public var ignore: Double? = 0.0
+    
+    // MARK: Shared Instance
+    
+    class func sharedInstance() -> CoinData {
+        struct Singleton {
+            static var sharedInstance = CoinData()
+        }
+        return Singleton.sharedInstance
+    }
+}
