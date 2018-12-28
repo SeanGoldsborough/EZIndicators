f35ac379b1fd6875dbd95e0cd1faafbd735dbc61
Sean Goldsborough <smgoldsborough@gmail.com>
2018-12-28 14:54:31 -0500

    feat: adds files for data model and api

---
+++ EZIndicators/BinanceAPI.swift
@@ -0,0 +1,10 @@
+//
+//  BinanceAPI.swift
+//  EZIndicators
+//
+//  Created by Sean Goldsborough on 12/28/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+//TODO: put funcs for API call to get data for each time period needed here
---
+++ EZIndicators/CurrencyData.swift
@@ -0,0 +1,10 @@
+//
+//  CurrencyData.swift
+//  EZIndicators
+//
+//  Created by Sean Goldsborough on 12/28/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+// TODO: put a shared instance singleton struct data model here
