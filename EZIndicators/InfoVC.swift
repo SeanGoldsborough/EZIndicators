f135157ab43847023ca9f376603ae7a33bba700c
Sean Goldsborough <smgoldsborough@gmail.com>
2018-12-28 14:40:06 -0500

    feat: adds mainVC and infoVC files

---
+++ EZIndicators/InfoVC.swift
@@ -0,0 +1,20 @@
+//
+//  InfoVC.swift
+//  EZIndicators
+//
+//  Created by Sean Goldsborough on 12/28/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ EZIndicators/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  EZIndicators
+//
+//  Created by Sean Goldsborough on 12/28/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
